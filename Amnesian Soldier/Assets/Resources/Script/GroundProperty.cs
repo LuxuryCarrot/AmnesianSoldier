78bf30051953f31c44fb98b1bae2bd2056406b39
LuxuryCarrot <dongkeun.lee@gmail.com>
2020-06-04 18:20:40 +0900

    dd

---
+++ Amnesian Soldier/Assets/Resources/Script/GroundProperty.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundProperty : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("GroundFall", 2.0f);
+        }
+    }
+
+    private void GroundFall()
+    {
+        Rigidbody rigi = gameObject.GetComponent<Rigidbody>();
+
+        rigi.isKinematic = false;
+    }
+}
