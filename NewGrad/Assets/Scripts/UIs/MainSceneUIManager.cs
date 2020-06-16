a7ee23847f04f79201bd4efd5b65c589c4ffccad
LuxuryCarrot <dongkeun.lee@gmail.com>
2020-06-16 23:13:22 +0900

    TestScene ADD & Scene name renamed

---
+++ NewGrad/Assets/Scripts/UIs/MainSceneUIManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneUIManager : MonoBehaviour
+{
+    public void NewGamePressed()
+    {
+        SceneManager.LoadScene("3_InGameScene");
+    }
+}
