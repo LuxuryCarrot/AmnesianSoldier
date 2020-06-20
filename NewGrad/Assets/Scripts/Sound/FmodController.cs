ce6580a4d60225438bf6a6776b5fde47c7420fa4
DKLee <dongkeun.lee@gmail.com>
2020-06-21 01:12:56 +0900

    Audio resource Added

---
+++ NewGrad/Assets/Scripts/Sound/FmodController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FmodController : MonoBehaviour
+{
+    public void PlayerRunSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PC/Run/Dirt");
+    }
+
+    public void PlayerBAttack()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PC/Attack/Sword/BAttack");
+    }
+
+    public void PlayerRAttack()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PC/Attack/Sword/RAttack");
+    }
+
+    public void PlayerPotion()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PC/Use/HPPotion");
+    }
+
+    public void PlayerDamaged()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PC/Damaged/Damaged");
+    }
+
+    public void PlayerDSword()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PC/Attack/Sword/DSword");
+    }
+
+    public void SlimeAttack()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Slime/Attack");
+    }
+
+    public void SlimeDie()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Slime/Die");
+    }
+
+    public void OnionAppear()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Onion/Appear");
+    }
+
+    public void OnionAttack()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Onion/Attack");
+    }
+
+    public void OnionDie()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Onion/Die");
+    }
+}
