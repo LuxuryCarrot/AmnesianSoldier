40ed6dff5813da0f7a53902fe56e192962045b94
kauthor <54879926+kauthor@users.noreply.github.com>
2020-07-13 17:29:52 +0900

    dd

---
+++ Amnesian_soldier/Assets/DeveloperOnly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperOnly : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            if (StageManager.stageSingletom.current != StageState.BOSSBATTLE)
+            {
+                StageManager.stageSingletom.SetState(StageState.MAPSELECT);
+                PlayerManager.playerSingleton.SetState(PlayerState.DELAY);
+                PlayerManager.playerSingleton.transform.position = new Vector3(MapPositionManager.mapMax, 2, 0);
+                MapPositionManager.mapPos = MapPositionManager.mapMax;
+            }
+            else
+                PlayerManager.playerSingleton.boss.SetState(BossState.DIE);
+        }
+    }
+}
