bf43bc51eb9878e06df8de74423b93ad5c3e401e
pineplum <ck.pineplum@gmail.com>
2020-07-14 10:14:37 +0900

    dd

---
+++ Amnesian_soldier/Assets/ButtonMainScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMainScene : ButtonParent
+{
+    public override void Execute()
+    {
+        base.Execute();
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+}
